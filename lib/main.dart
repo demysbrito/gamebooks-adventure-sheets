e1aa4a3a4b72b2402f9b80f64552f6932cd257e5
Mateus Pedroso <mateuspsilva.dev@gmail.com>
2023-06-24 02:53:08 -0300

    add game books list in the home page

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,67 @@ class _MyHomePageState extends State<MyHomePage> {
   int dice1 = 0;
   int dice2 = 0;
   bool visible = false;
+  List<String> books = [
+    'The Warlock of Firetop Mountain',
+    'The Citadel of Chaos',
+    'The Forest of Doom',
+    'Starship Traveller',
+    'City of Thieves',
+    'Deathtrap Dungeon',
+    'Island of the Lizard King',
+    'Scorpion Swamp',
+    'Caverns of the Snow Witch',
+    'House of Hell',
+    'Talisman of Death',
+    'Space Assassin',
+    'Freeway Fighter',
+    'Temple of Terror',
+    'The Rings of Kether',
+    'Seas of Blood',
+    'Appointment with F.E.A.R.',
+    'Rebel Planet',
+    'Demons of the Deep',
+    'Sword of the Samurai',
+    'Trial of Champions',
+    'Robot Commando',
+    'Masks of Mayhem',
+    'Creature of Havoc',
+    'Beneath Nightmare Castle',
+    'Crypt of the Sorcerer',
+    'Star Strider',
+    'Phantoms of Fear',
+    'Midnight Rogue',
+    'Chasms of Malice',
+    'Battleblade Warrior',
+    'Slaves of the Abyss',
+    'Sky Lord',
+    'Stealer of Souls',
+    'Daggers of Darkness',
+    'Armies of Death',
+    'Portal of Evil',
+    'Vault of the Vampire',
+    'Fangs of Fury',
+    'Dead of Night',
+    'Master of Chaos',
+    'Black Vein Prophecy',
+    'The Keep of the Lich-Lord',
+    'Legend of the Shadow Warriors',
+    'Spectral Stalkers',
+    'Tower of Destruction',
+    'The Crimson Tide',
+    'Moonrunner',
+    'Siege of Sardath',
+    'Return to Firetop Mountain',
+    'Island of the Undead',
+    'Night Dragon',
+    'Spellbreaker',
+    'Legend of Zagor',
+    'Deathmoor',
+    'Knights of Doom',
+    'Magehunter',
+    'Revenge of the Vampire',
+    'Curse of the Mummy',
+  ];
 
   int rollDice() {
     var value = Random();
@@ -68,7 +129,26 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
+          child: ListView.builder(
+        itemCount: books.length,
+        itemBuilder: (BuildContext context, int index) {
+          final item = books[index];
+          return ListTile(
+            title: Text(item),
+          );
+        },
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: rollDice,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+/*
+Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Visibility(
@@ -120,12 +200,4 @@ class _MyHomePageState extends State<MyHomePage> {
                 onPressed: rollTwoDices, child: const Text('Rolar 2 dados'))
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: rollDice,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
+*/
