c5fa08ddca043e60c32a0a8110bded66517fc736
Mateus Pedroso <mateuspsilva.dev@gmail.com>
2023-06-28 02:17:21 -0300

    fix: rules for initial status fixed

--- lib/util/character_attributes.dart
+++ lib/util/character_attributes.dart
@@ -1,16 +1,16 @@
 import 'package:gamebooks_adventure_sheets/util/dice_roll.dart';
 
 int setSkill(skillValue) {
-  skillValue = rollDice(skillValue) + rollDice(skillValue);
+  skillValue = rollDice(skillValue) + 6;
   return skillValue;
 }
 
 int setStamina(staminaValue) {
-  staminaValue = rollDice(staminaValue) + rollDice(staminaValue);
+  staminaValue = rollDice(staminaValue) + rollDice(staminaValue) + 12;
   return staminaValue;
 }
 
 int setLuck(luckValue) {
-  luckValue = rollDice(luckValue) + rollDice(luckValue);
+  luckValue = rollDice(luckValue) + 6;
   return luckValue;
 }
